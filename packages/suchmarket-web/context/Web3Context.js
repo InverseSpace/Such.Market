e84a9fb3a66f39c1b2687f6db9ff3999460aa873
MXJXN <modulations@gmail.com>
2020-08-28 10:59:19 -0400

    WIP Web3Context

---
+++ packages/suchmarket-web/context/Web3Context.js
@@ -0,0 +1,16 @@
+import React, { createContext, useEffect } from 'react';
+import Web3 from 'web3';
+import Web3Modal from 'web3modal';
+
+export const Web3Context = createContext({
+  ethersProvider: null,
+  connectWeb3: async () => {},
+  disconnect: () => undefined,
+});
+
+export const Web3ContextProvider = ({ children }) => {
+  const [web3modal, setWeb3Modal] = useState();
+  useEffect(() => {
+    // setWeb3Modal(new Web3Modal({}))
+  });
+};
--- packages/suchmarket-web/pages/index.js
+++ packages/suchmarket-web/pages/index.js
@@ -4,7 +4,16 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
     <div className={styles.container}>
-      
+      <h1>SUCH.MARKET</h1>
+      <div>
+        Sign in
+      </div>
+      <div>
+        <a href="#">List your service</a>
+      </div>
+      <div>
+          services for sale
+      </div>
     </div>
   )
 }
